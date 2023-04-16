14893104204293038dd3fb42b1e9af19d6fea563
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2023-04-16 14:59:27 +0000

    installs colord, adds function description for applyTheme

--- color-themer.js
+++ color-themer.js
@@ -1,5 +1,10 @@
-const generate = () => {
-  return "hello";
-};
+import { colord } from "colord";
 
-module.exports = generate;
+// applyTheme: function that applies a theme to the document
+// takes the following arguments:
+//  - theme: the theme to apply
+//  - duration: the duration of the transition
+//  - onFinished: a callback to call when the transition is finished: passes the new theme object as an argument
+//  - onProgress: a callback to call during each frame of the transition: passes the theme object and the progress (0-1) as arguments
+
+// module.exports = generate;
